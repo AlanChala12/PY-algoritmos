03bb9dfec282683974d68b4e7c7a5bab2d9d768f
Alan Chala <alan.chala12@gmail.com>
2024-11-02 20:29:23 -0500

    Nueva sección de algoritmos.

---
+++ Bucles_y_condicionales/cantidad_de_digitos.py
@@ -0,0 +1,28 @@
+# Leer dos números enteros y determinar cuál de los dos tiene más dígitos.
+number_one = int(input('Ingresa un número entero: '))
+number_two = int(input('Ingresa otro número entero: '))
+
+digitos_number_one = 0
+digitos_number_two = 0
+
+"""Cantidad de dígitos del número uno."""
+while number_one != 0:
+
+  number_one = int(number_one / 10)
+  digitos_number_one = digitos_number_one + 1
+
+"""Cantidad de dígitos del número dos."""
+while number_two != 0:
+
+  number_two = int(number_two / 10)
+  digitos_number_two = digitos_number_two + 1
+
+
+if digitos_number_one > digitos_number_two:
+  print('El primero número tiene mas dígitos que el segundo número.')
+elif digitos_number_two > digitos_number_one:
+  print('El segundo número tiene mas dígitos que el primer número.')
+else:
+  print('Ambos tienen la misma cantidad de dígitos.')
+
+
