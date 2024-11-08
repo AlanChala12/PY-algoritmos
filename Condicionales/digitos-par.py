c4b4db6a54f15dce45ffb79549ad491f338ab60a
Alan Chala <alan.chala12@gmail.com>
2024-11-08 18:13:40 -0500

    Leer un número entero de dos dígitos y determinar si ambos dígitos son pares.

---
+++ Condicionales/digitos-par.py
@@ -0,0 +1,15 @@
+user_name = int(input('Ingresa un número entero positivo de dos dígitos: '))
+
+if user_name < 10:
+  user_name += 10
+
+elif user_name > 10:
+  dig1 = int(user_name / 10)
+
+  dig2 = int(user_name / 10) * 10
+  dig2 = user_name - dig2
+
+  if dig1 % 2 == 0 and dig2 % 2 == 0:
+    print(f'Ambos dígitos del número {user_name}, son pares.')
+  else:
+    print(f'Solo uno o ningúno de los dígitos del número {user_name}, son pares.')
