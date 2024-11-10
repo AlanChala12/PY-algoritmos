f15c0b32aa4a967b2e525d97343a7de09af206ec
Alan Chala <alan.chala12@gmail.com>
2024-11-09 19:43:18 -0500

    Leer un número entero de dos dígitos y determinar si es primo y además si es negativo.

---
+++ Condicionales/primo-y-negativo.py
@@ -0,0 +1,14 @@
+x = int(input('Ingresa un número: '))
+
+if x >= 1:
+
+  xx = x / x
+  yy = x / 1
+
+  if xx == 1 and yy == x:
+    print(f'El número {x}, es primo.')
+  else:
+    print(f'El número {x}, no es primo.')
+
+else:
+  print(f'El número {x}, no es primo ya que no cumple la primera condición de los números primos "n > 1"')
