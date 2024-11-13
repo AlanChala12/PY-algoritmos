80146f24ebf6a38f2d9ea1a50f6a3088670a8945
Alan Chala <alan.chala12@gmail.com>
2024-11-13 17:59:55 -0500

    Leer dos números enteros y determinar cuál es el mayor

--- Condicionales/example8.py
+++ Condicionales/example8.py
@@ -1,3 +1,5 @@
+# Leer dos números enteros y determinar cuál es el mayor.
+
 print("---Verificación de igualdad de dos números---")
 print()
 number1 = int(input('Ingresa un número entero: '))
@@ -5,7 +7,9 @@ number2 = int(input('Ingresa otro número entero: '))
 
 if number1 > number2: 
   print(f'{number1} es mayor que {number2}.')
+  
 elif number2 > number1:
   print(f'{number2} es mayor que {number1}.')
+  
 else:
   print('Ambos números son iguales.')
