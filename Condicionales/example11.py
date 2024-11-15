b5f89efbede6aea28b5f8435419775d04ed1320e
Alan Chala <alan.chala12@gmail.com>
2024-11-15 12:42:34 -0500

    Leer un número entero y determinar si es negativo.

---
+++ Condicionales/example11.py
@@ -0,0 +1,8 @@
+# Leer un número entero y determinar si es negativo. 
+number = int(input('Ingresa un número entero: '))
+
+if number < 0:
+    print('--Es un número negativo--')
+    
+else:
+    print('--Es un número positivo--')
