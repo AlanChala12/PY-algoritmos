df3ef8340a6f6c5e0a5db839dc2405049eb74379
Alan Chala <alan.chala12@gmail.com>
2024-11-15 12:39:44 -0500

    Leer un número entero y determinar si es un número terminado en 4.

--- Condicionales/example9.py
+++ Condicionales/example9.py
@@ -6,10 +6,10 @@ try:
   ultimo_digito = user_number - ultimo_digito
 
   if ultimo_digito == 4:
-    print('--El número proporcionado termina en 4--')
+    print('---El número proporcionado termina en 4---')
 
   else:
-    print('--El número proporcionado no termina en 4--')
+    print('---El número proporcionado no termina en 4---')
 
 except:
-  print('--FORMATO INVALIDO--')
+  print('---FORMATO INVALIDO---')
