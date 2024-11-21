90b72e349ff5daadc4b71331db62e35a6c93f802
Alan Chala <alan.chala12@gmail.com>
2024-11-21 13:31:52 -0500

    Leer un número entero de 3 dígitos y determinar si tiene el dígito 1.

---
+++ Condicionales/example21.py
@@ -0,0 +1,11 @@
+# Leer un número entero de 3 dígitos y determinar si tiene el dígito 1.
+x = int(input('Ingresa un entero mayor o igual a cien: '))
+x = str(x)
+    
+if len(x) == 3:
+    if "1" in x:
+        print('El número proporcionado tiene el dígito uno.')
+    else:
+        print('El número proporcionado no tiene el dígito uno.')
+else:
+    print('El número ingresado no tiene tres dígitos.')
