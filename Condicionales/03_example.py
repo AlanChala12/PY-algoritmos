643e2204f7ebf2e009a055dce6104b1da6643a44
Alan Chala <alan.chala12@gmail.com>
2024-11-26 14:03:14 -0500

    Leer un número entero de dos dígitos y determinar si los dos dígitos son iguales.

--- Condicionales/03_example.py
+++ Condicionales/03_example.py
@@ -1,13 +1,13 @@
 # Leer un número entero de dos dígitos y determinar si los dos dígitos son iguales.
+number = int(input("Entero: "))
 
-number = int(input("Ingresa un número entero de dos dígitos: "))
+dig1 = number // 10
+dig2 = number % 10
 
-dig1 = int(number / 10)
+if number < 10:
+  print(f'--El número debe ser mayor o igual a diez--')
 
-dig2 = int(number / 10) * 10
-dig2 = number - dig2
-
-if dig1 == dig2:
+elif dig1 == dig2:
   print(f'Los dígitos que conforman al número {number}, son iguales.')
   
 else: 
