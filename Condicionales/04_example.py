7b0be628a54a702b30103b9884d6df48004dcf20
Alan Chala <alan.chala12@gmail.com>
2024-11-26 14:04:57 -0500

    Leer un número entero de dos dígitos y determinar si sus dos dígitos son primos.

--- Condicionales/04_example.py
+++ Condicionales/04_example.py
@@ -1,22 +1,14 @@
 # Leer un número entero de dos dígitos y determinar si sus dos dígitos son primos.
-
-
 number = int(input('Ingresa un número mayor o igual a 10: '))
 
-"""Se obtiene el primer dígito."""
-dig1 = int(number / 10)
-
-"""Se obtiene el segundo dígito"""
-dig2 = int(number / 10) * 10
-dig2 = number - dig2
+dig1 = number // 10
+dig2 = number % 10
 
 if number >= 10:
 
-  #Verificar si el primer dígito es primo.
   x1 = dig1 / dig1
   xx1 = dig1 / 1
 
-  #Verificar si el segundo dígito es primo.
   y2 = dig2 / dig2
   yy2 = dig2 / 1
 
