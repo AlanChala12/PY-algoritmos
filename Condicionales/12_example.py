80c68323d92d8281ab8e14492d01bf924bc07e37
Alan Chala <alan.chala12@gmail.com>
2024-12-03 12:39:02 -0500

    Leer un número entero de dos dígitos y determinar si un dígito es múltiplo del otro.

--- Condicionales/12_example.py
+++ Condicionales/12_example.py
@@ -6,15 +6,13 @@ dig1 = number // 10
 dig2 = number % 10 
 
 if number < 10 or number >= 100:
-    print('--Debe ser un número mayor o igual que 10 y menor que 100--')
+    print('--Debe ser un número mayor o igual a 10 y menor que 100--')
 
 elif dig1 % dig2 == 0:
-    print(f'--El número {dig2} es múltiplo de {dig1}.')
+    print(f'El número {dig2} es múltiplo de {dig1}.')
 
 elif dig2 % dig1 == 0:
-    print(f'--El número {dig1} es múltiplo de {dig2}.')
+    print(f'El número {dig1} es múltiplo de {dig2}.')
 
 else:
     print('--Ningún número es múltiplo del otro--')
-    
-    
