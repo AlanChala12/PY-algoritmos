e228e55aefc74be84a5dba0b5e23778038af9e65
Alan Chala <alan.chala12@gmail.com>
2024-12-04 19:13:09 -0500

    Leer un número entero y determina si es igual a 10

---
+++ Condicionales/25_example.py
@@ -0,0 +1,7 @@
+# Leer un número entero y determina si es igual a 10 
+entero = int(input('Entero: '))
+
+if entero == 10:
+  print(f'El número {entero} es igual a 10.')
+else:
+  print(f'El número {entero} no es igual a 10.')
