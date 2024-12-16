d288fc665241e475a9f5dba787cfeb0d681446bf
Alan Chala <alan.chala12@gmail.com>
2024-12-16 12:17:40 -0500

    Construir una función que reciba como parámetros dos números enteros y retorne 1 si el primer número es múltiplo del segundo y 0 si no.

---
+++ Funciones/09_example.py
@@ -0,0 +1,15 @@
+# Construir una función que reciba como parámetros dos números enteros y retorne 1 si el primer número es múltiplo del segundo y 0 si no.
+def multiplo(n1, n2):
+
+  if n1 <= 0 or n2 <= 0:
+    return '--Ingresa un número positivo o mayor que cero--'
+
+  else:
+    if n1 % n2 == 0:
+      return 1
+    else:
+      return 0
+  
+entero_uno = int(input('Ingresa un número entero:'))
+entero_dos = int(input('Ingresa otro número entero: '))
+print(multiplo(entero_uno, entero_dos))
