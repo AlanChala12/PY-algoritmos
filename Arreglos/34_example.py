7f22b7b6d3277735973f32afc54756e33de9625a
Alan Chala <alan.chala12@gmail.com>
2025-03-17 11:05:06 -0500

    Dada un conjunto de estudiantes, determinar el promedio de notas de un estudiante es especifico.

--- Arreglos/34_example.py
+++ Arreglos/34_example.py
@@ -3,7 +3,8 @@
 student_marks = {
     'Krishna':[ 67, 68, 69 ],
     'Arjun': [ 70, 98, 63 ],
-    'Malika': [ 52, 56, 60 ]
+    'Malika': [ 52, 56, 60 ],
+    'Luca': [90, 100, 20]
 }
 
 student = input('Ingresa el nombre del estudiante: ')
