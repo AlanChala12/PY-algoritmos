3a5faea0d4a4fa7fdf4478dfa84456b6895b331a
Alan <alan.chala12@gmail.com>
2025-03-31 11:12:23 -0500

    Leer 10 números enteros, almacenarlos en un vector y calcularle el factorial a cada uno de los números leídos almacenándolos en otro vector.

--- Arreglos/06_example.py
+++ Arreglos/06_example.py
@@ -1,17 +1,13 @@
 # Leer 10 números enteros, almacenarlos en un vector y calcularle el factorial a cada uno de los números leídos almacenándolos en otro vector.
 
-numeros = []
+numeros = [ int(input(f'Entero { y + 1 }: ')) for y in range(10) ]
 factoriales = []
-
-for y in range(1, 11):
-    numero = int(input(f'Entero {y}: '))
-    numeros.append(numero)
 
 for iterador in numeros:
     x = iterador
     factorial = x
     
-    for i in range(1, x):
+    for i in range( 1, x ):
         factorial = factorial * i
         
     factoriales.append(factorial)
