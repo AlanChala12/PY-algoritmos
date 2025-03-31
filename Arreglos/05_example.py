5b3de2f79145233eb293f02cc0100950c3b8ee1a
Alan <alan.chala12@gmail.com>
2025-03-31 11:09:35 -0500

    Leer 10 números enteros, almacenarlos en un vector y determinar cuántas veces está repetido el mayor.

--- Arreglos/05_example.py
+++ Arreglos/05_example.py
@@ -1,15 +1,15 @@
 # Leer 10 números enteros, almacenarlos en un vector y determinar cuántas veces está repetido el mayor.
-enteros = []
-contador = 0
 
-for i in range(1, 11):
-  numeros = int(input(f'Entero {i}: '))
-  enteros.append(numeros)
+try:
+  enteros = [ int(input(f'Entero {i + 1}: ')) for i in range(10) ]
+  apariciones = 0
+  numero_mayor = max( enteros )
 
-numero_mayor = max(enteros)
-
-for x in enteros:
+  for x in enteros:
     if x == numero_mayor:
-        contador += 1
-    
-print(f'El número {numero_mayor} que es el número mayor se encuentra {contador} vez/veces')
+      apariciones += 1
+      
+  print(f'El número mayor {numero_mayor}, del vector { enteros }, se encuentra { apariciones } vez/veces')
+  
+except ValueError:
+  print('Por favor, ingresa valores numéricos')
