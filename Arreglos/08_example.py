dd0632d0005bdb5dbe402aa46042c0346865881b
Alan <alan.chala12@gmail.com>
2025-04-05 13:07:44 -0500

    Leer 10 números enteros, almacenarlos en un vector y determinar en qué posiciones se encuentra el número mayor.

--- Arreglos/08_example.py
+++ Arreglos/08_example.py
@@ -1,9 +1,5 @@
 # Leer 10 números enteros, almacenarlos en un vector y determinar en qué posiciones se encuentra el número mayor.
-numeros = []
+numeros = [ int(input(f'Entero { i + 1 }: ')) for i in range(10) ]
+max_num = max( numeros )
 
-for i in range(0, 11):
-    numero = int(input(f'Entero {i}: '))
-    numeros.append(numero)
-    
-max_num = max(numeros)
 print(f'El número mayor "{max_num}" se encuentra en el indice {numeros.index(max_num)}')
