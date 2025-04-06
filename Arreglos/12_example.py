2901c4e089f5c11cb81fdf1434441b12f9efe217
Alan <alan.chala12@gmail.com>
2025-04-06 16:37:51 -0500

    Leer 10 números enteros, almacenarlos en un vector y determinar si el promedio entero de estos datos está almacenado en el vector.

--- Arreglos/12_example.py
+++ Arreglos/12_example.py
@@ -1,20 +1,9 @@
 # Leer 10 números enteros, almacenarlos en un vector y determinar si el promedio entero de estos datos está almacenado en el vector.
 
-numeros = []
-sum_de_digitos = 0
-n = 10
+numeros = [ int(input(f'Entero {i + 1}: ')) for i in range( 10 ) ]
+promedio = max( numeros ) // len( numeros )
 
-for i in range(1, 11):
-    numero = int(input(f'Entero {i}: '))
-    numeros.append(numero)
-    
-for x in numeros:
-    sum_de_digitos = sum_de_digitos + x 
-    
-promedio_entero = sum_de_digitos // n
-
-if promedio_entero in numeros:
-    print(f'El promedio entero "{promedio_entero}" se encuentra en el vector {numeros}')
+if promedio in numeros:
+    print(f'El promedio entero { promedio } se encuentra en el vector { numeros }')
 else:
-    print(f'El promedio entero "{promedio_entero}" no se encuentra en el vector {numeros}')
-    
+    print(f'El promedio entero { promedio } NO SE encuentra en el vector { numeros }')
