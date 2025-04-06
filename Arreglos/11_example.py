881cfd96dcfe20ff429b943178ecc8125b320bd3
Alan <alan.chala12@gmail.com>
2025-04-06 16:34:23 -0500

    Leer 10 números enteros, almacenarlos en un vector y determinar a cuánto es igual el promedio entero de los datos del vector.

--- Arreglos/11_example.py
+++ Arreglos/11_example.py
@@ -1,14 +1,6 @@
-# Leer 10 números enteros, almacenarlos en unvector y determinar a cuánto es igual el promedio entero de los datos del vector.
-numeros = []
-sum_de_digitos = 0
-n = 10
+# Leer 10 números enteros, almacenarlos en un vector y determinar a cuánto es igual el promedio entero de los datos del vector.
 
-for i in range(1, 11):
-    numero = int(input(f'Entero {i}: '))
-    numeros.append(numero)
-    
-for x in numeros:
-    sum_de_digitos = sum_de_digitos + x 
-    
-promedio_entero = sum_de_digitos // n
-print(f'El promedio entero de {numeros} es de "{promedio_entero}"')
+numeros = [ int(input(f'Entero { i + 1 }: ')) for i in range( 10 ) ]
+promedio = sum( numeros ) // len( numeros )
+
+print(f'El promedio entero del vector { numeros }, es: { promedio }')
