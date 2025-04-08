abde0afb0c555db862b44e22c1f1cafe2812eafa
Alan <alan.chala12@gmail.com>
2025-04-08 10:26:37 -0500

    Leer 10 números enteros, almacenarlos en un vector y determinar cuántos números negativos hay.

--- Arreglos/16_example.py
+++ Arreglos/16_example.py
@@ -1,16 +1,14 @@
 # Leer 10 números enteros, almacenarlos en un vector y determinar cuántos números negativos hay.
-enteros = []
-negativos = 0
 
-for i in range(1, 11):
-  numeros = int(input(f'Entero {i}: '))
-  enteros.append(numeros)
+enteros = [ int(input(f'Entero { i + 1 }: ')) for i in range( 10 ) ]
+negativos = 0
 
 for x in enteros:
   if x < 0:
     negativos += 1
-
-if negativos == 0:
-  print(f'No hay número negativos en el vector {enteros}')
+    
+if negativos != 0:
+  print(f'Hay { negativos } números negativos en el vector { enteros }')
+  
 else:
-  print(f'Hay {negativos} números negativos en el vector')
+  print(f'No se encuentran números negativos en el vector { enteros }')
