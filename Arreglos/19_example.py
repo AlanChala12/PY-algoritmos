9b142324f64feb99baf625ef88a7ba33e37523b9
Alan <alan.chala12@gmail.com>
2025-04-11 14:59:41 -0500

    Leer 10 números enteros, almacenarlos en un vector y determinar cuántas veces en el vector se encuentra el dígito 2

--- Arreglos/19_example.py
+++ Arreglos/19_example.py
@@ -1,14 +1,10 @@
-# Leer 10 números enteros, almacenarlos en un vector y determinar cuántas veces en el vector se encuentra el dígito 2.
-enteros = []
-cantidad_de_dos = 0
+# Leer 10 números enteros, almacenarlos en un vector y determinar cuántas veces en el vector se encuentra el dígito 2
 
-for i in range(1, 11):
-    numero = int(input(f'Entero {i}: '))
-    enteros.append(numero) 
+enteros = [ int(input(f'Entero {i + 1}: ')) for i in range(10) ]
+apariciones = 0
 
-for x in enteros:
-    conver = str(x)
-    cantidad_de_dos += conver.count('2')
-
-print(f'El dígito 2 se encuentra {cantidad_de_dos} vez/veces en el vector.')
-print(enteros)
+for i in enteros:
+    num = str( i )
+    apariciones += num.count('2')
+    
+print(f'El dígito 2 se encuentra { apariciones } vez/veces en el vector.')
