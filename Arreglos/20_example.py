1a2fc096f7818987dd3662522fed42903a1b8844
Alan <alan.chala12@gmail.com>
2025-04-11 15:04:21 -0500

    Leer 10 números enteros, almacenarlos en un vector y determinar si el promedio entero de dichos números es un número primo.

--- Arreglos/20_example.py
+++ Arreglos/20_example.py
@@ -1,25 +1,16 @@
 # Leer 10 números enteros, almacenarlos en un vector y determinar si el promedio entero de dichos números es un número primo.
-enteros = []
-sum_digitos = 0
-n = 10
 
-for i in range(1, 11):
-  numeros = int(input(f'Entero {i}: '))
-  enteros.append(numeros) 
+enteros = [ int(input(f'Entero {i + 1}: ')) for i in range( 10 ) ]
+promedio = sum( enteros ) // len( enteros )
+esPrimo = True
 
-for x in enteros:
-  sum_digitos = sum_digitos + x
-
-promedio = sum_digitos // n
-
-is_primo = True
-for primo in range(2, promedio):
-  if promedio % primo == 0:
-    is_primo = False
+for i in range( 2, promedio ):
+  if promedio % i == 0:
+    esPrimo = False
     break
-
-if is_primo == True and is_primo > 1:
-  print(f'El promedio entero {promedio} del vector {enteros}, es un número ¡PRIMO!')
-
+  
+if esPrimo:
+  print(f'El promedio entero del vector { enteros }, ES un número primo')
 else:
-  print(f'El promedio entero {promedio} del vector {enteros}, no es un número ¡PRIMO!')
+  print(f'El promedio entero { promedio } del vector { enteros }, NO ES un número primo')
+  
