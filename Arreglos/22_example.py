5383f692c5812a81f6032308be32dab685b313c0
Alan <alan.chala12@gmail.com>
2025-04-12 13:38:06 -0500

    Leer 10 números enteros, almacenarlos en un vector y determinar en qué posición está el número con más dígitos.

--- Arreglos/22_example.py
+++ Arreglos/22_example.py
@@ -1,13 +1,6 @@
 # Leer 10 números enteros, almacenarlos en un vector y determinar en qué posición está el número con más dígitos.
-enteros = []
 
-for i in range(1, 11):
-  numeros = int(input(f'Entero {i}: '))
-  enteros.append(numeros)
+enteros = [ int(input(f'Entero { i + 1 }: ')) for i in range( 10 ) ]
+maxNum = max( enteros )
 
-numero_mayor = max(enteros)
-
-if numero_mayor in enteros:
-    print(f'El número con más dígitos "{numero_mayor}" se encuentra en el índice {enteros.index(numero_mayor)}')
-
-print(enteros)
+print(f'Número con más dígitos: { maxNum } - Posición: { enteros.index( maxNum ) + 1 }')
