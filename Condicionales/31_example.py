bc20dc50c02c69faed1c1edff1f5393ed5bbf104
Alan <alan.chala12@gmail.com>
2025-04-16 21:01:21 -0500

    Leer un número entero de cuatro dígitos y determinar a cuánto es igual la suma de sus dígitos.

--- Condicionales/31_example.py
+++ Condicionales/31_example.py
@@ -1,14 +1,13 @@
 # Leer un número entero de cuatro dígitos y determinar a cuánto es igual la suma de sus dígitos.
-entero = int(input('Entero: '))
 
-if entero < 1000 or entero > 10000:
-  print('Por favor, ingresa un número que contenga cuatro dígitos.')
+num = int(input('Entero: '))
 
+if 1000 <= num < 10000:
+  sumNum = 0
+  for i in str(num):
+    sumNum = sumNum + int(i)
+  print(f'Suma de dígitos del número { num }, es: { sumNum }')
+  
 else:
-  dig1 = entero // 1000
-  dig2 = (entero // 100) % 10
-  dig3 = (entero // 10) % 10
-  dig4 = entero % 10
+  print('Ingresa un número que contenga cuatro dígitos')
 
-  result = dig1 + dig2 + dig3 + dig4
-  print(f'La suma de los dígitos del número {entero}, es igual a {result}')
