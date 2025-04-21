2a52fb26ad1f674f005ee1d35e13013c2b713c3e
Alan <alan.chala12@gmail.com>
2025-04-20 20:20:57 -0500

    Leer un número entero y determinar si termina en 7.

--- Condicionales/27_example.py
+++ Condicionales/27_example.py
@@ -1,8 +1,12 @@
 # Leer un número entero y determinar si termina en 7. 
-entero = int(input('Entero: '))
-ultimo_digito = entero % 10
 
-if ultimo_digito == 7:
-  print(f'El número {entero} termina en 7.')
-else:
-  print(f'El número {entero} no termina en 7.')
+try:
+  entero = int(input('Número: '))
+
+  if entero % 10 == 7:
+    print('Termina en siete')
+  else:
+    print('No termina en siete')
+    
+except ValueError:
+  print('Ingresa valores numéricos')
